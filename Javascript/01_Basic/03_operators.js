bd49c7c27154a713a5cdbd9db8ef51a74a119056
SharatRokkam <rokkamsharath1@gmail.com>
2025-02-25 10:05:14 +0530

    operator

---
+++ Javascript/01_Basic/03_operators.js
@@ -0,0 +1,78 @@
+// What are Operators?
+// Operands and Operators
+// Operators in Javascript are symbols that are used to perform operations on variables and values
+
+// 1. Arithmetic Operator
+// 2. Assignment Operator
+// 3. Logical Operator
+// 4. Comparison Operator
+// 5. String Operator
+// 6. Ternary Operator
+// 7. typeof and instanceOf
+// 8. Bitwise Operator
+
+// Arithmetic Operator : are used to perform mathematical operations
+// Example : +, -, *, %, /, **
+
+// console.log(4 + 5);
+// console.log(5 - 3);
+// console.log(5 * 3);
+// console.log(10 % 2);
+// console.log(10 / 2);
+// console.log(3 ** 2);
+
+// Assignment Operator : used to assign values to variables
+// Example : = (equal to), +=, -=, *=, /=, %= , **=
+
+// let x = 10;
+
+// console.log();
+
+// x != 10
+// x += 10
+// x = x + 10
+// // x -= 10
+// x = x - 10
+// // x *= 10
+// x = x * 10
+
+// Comparison Operator : used to compare two or more values and return in true or false
+
+// Example : ==(double equal to), ===(strictly equal to) , !=, !==, <, >, >=, <=
+
+//imp : == and ===, == it checks just the value but === it compares or checks both data type and value
+// console.log(10 == "10");
+// console.log(10 === "10");
+// console.log(10 != "10");
+// console.log(10 !== "10");
+// console.log(40 > 2);
+// console.log(40 >= "40");
+// console.log(40 <= 40);
+// console.log(40 < 4);
+
+//type conversion and type coercian
+
+//Logical Operator : &&, ||, !
+// console.log(true && false);
+// console.log(true || false);
+// let a = 10;
+// console.log(!a);
+
+// Ternary Operator : check the condition
+// condition ? true : false
+
+// let age = 23;
+
+// let result = age > 20 ? "you can drive" : "you cannot drive";
+// console.log(result);
+
+// typeOf and instanceof
+
+// console.log(typeof "10")
+
+// String Operator
+
+// let firstName = "Something";
+// let lastName = "    ";
+
+// console.log(firstName + lastName);
