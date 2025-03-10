1a3b61e26aa0d7d97440235f786624a952ed943a
SharatRokkam <rokkamsharath1@gmail.com>
2025-03-10 10:42:08 +0530

    Date and Time

---
+++ Javascript/01_Basic/07_Numbers.js
@@ -0,0 +1,65 @@
+// const score = 100;
+// console.log(average);
+
+// const balance = new Number(499);
+// console.log(balance)
+
+// const average = 88.20029482;
+
+// console.log(average.toFixed(2));
+
+// console.log(typeof average.toString());
+
+// const score = 1123.8322424;
+// console.log(score.toPrecision(4));
+
+// const accountBalance = 1000000;
+
+// console.log(accountBalance.toLocaleString("en-IN"));
+
+// toExponential
+
+// =====Math========
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.min(8, 3, 4, 5));
+// console.log(Math.max(8, 3, 4, 5));
+
+// console.log(Math.sqrt(125));
+
+//0 to 1
+// console.log(Math.round(Math.random() * 10));
+
+// 1 to 6
+// console.log(Math.round(Math.random() * 6) + 1);
+
+// Date and Time
+// const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.);
+
+// const myNewDate = new Date(2023, 1, 23, 5, 2);
+// const myNewDate = new Date("2023-01-23");
+// console.log(myNewDate.toLocaleString());
+
+// Time
+
+const myTimeStamp = new Date();
+// console.log(myTimeStamp.now());
+// console.log(myTimeStamp.getTime() / 1000);
+
+// console.log(myTimeStamp.getMonth() + 1);
+// console.log(myTimeStamp.getFullYear());
+// console.log(myTimeStamp.getHours());
+// console.log(myTimeStamp.getMinutes());
+// console.log(myTimeStamp.getSeconds());
+
+// console.log(myTimeStamp.getDay());
