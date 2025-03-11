f75c469c0c1c4d67eebbe0c7b619a4cfe6e2696e
SharatRokkam <rokkamsharath1@gmail.com>
2025-03-11 16:18:01 +0530

    string and execution context

---
+++ Javascript/01_Basic/08_Memory.js
@@ -0,0 +1,43 @@
+let num = 2;
+function cube(n) {
+  var answer = num * num * num;
+  return answer;
+}
+
+let cube2 = cube(num);
+let cube3 = cube(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Stack (Primitive) and Heap (Non-Primitive)
+
+// let myInstaHandle = "sharatRokkamdotcom";
+
+// let mySecondOne = myInstaHandle;
+// mySecondOne = "newInsta";
+
+// console.log(myInstaHandle);
+// console.log(mySecondOne);
+
+// let userOne = {
+//   email: "something@gmail.com",
+//   upiId: "upi@ybl",
+// };
+
+// let userTwo = userOne;
+// userTwo.email = "nothing@gmail.com";
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+//Js is single threaded synchronous language....
---
+++ Javascript/01_Basic/09_strings.js
@@ -0,0 +1,39 @@
+// let random = " 2";
+// console.log(myNameIs);
+
+// let myAnotherName = new String("John Doe");
+// console.log(myAnotherName);
+
+// console.log(myNameIs.length)
+// console.log(myNameIs[0]);
+
+// console.log(myNameIs.charAt(3));
+// console.log(myNameIs.toUpperCase());
+// console.log(myNameIs.toLowerCase());
+
+// console.log(myNameIs.concat(random));
+
+// console.log(myNameIs.includes("J"));
+
+// console.log(myNameIs.indexOf("C"));
+// console.log(myNameIs.lastIndexOf("A"));
+
+// console.log(myNameIs.replace("C", "A"));
+
+// let newString = "I am good boy, I do good stuff, I am not good in studies";
+
+// console.log(newString.replaceAll("good", "bad"));
+// console.log(newString.replace("good", "bad"));
+
+// console.log(myNameIs.split(""));
+// console.log(typeof myNameIs);
+
+let myNameIs = "JohnCena";
+
+// console.log(myNameIs.substring(4, 8));
+
+console.log(myNameIs.slice(-4));
+
+// at and charAt
+// replace and replaceAll
+//substring and slice
