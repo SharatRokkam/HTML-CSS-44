4c8691752813bdb4c1e09892d0d53a9536b8fdce
SharatRokkam <rokkamsharath1@gmail.com>
2025-03-21 10:39:51 +0530

    .

--- Javascript/02_Intermediate/01_this.js
+++ Javascript/02_Intermediate/01_this.js
@@ -34,20 +34,62 @@
 // console.log(user3);
 // console.log(user4);
 
-const person = { name: "Madhu" };
-const person2 = { name: "Srinu" };
+// const person = { name: "Madhu" };
+// const person2 = { name: "Srinu" };
 
-function greet(designation, age) {
-  console.log(
-    `Hello! Mr.${this.name}, you are good ${designation} developer, ${age}`
-  );
+// function greet(designation, age) {
+//   console.log(
+//     `Hello! Mr.${this.name}, you are good ${designation} developer, ${age}`
+//   );
+// }
+// //argument = individually
+// greet.call(person, "frontend", 23);
+// greet.call(person2, "backend", 24);
+// //arguments in an array
+// greet.apply(person, ["fullstack", 74]);
+
+// // bind - it has to be referred to a new variable
+// const greetPerson = greet.bind(person);
+// greetPerson("java", 50);
+
+// Real World Examples of call apply and bind
+
+// call is used when you want to pass values individually and execute the function immediately
+// const food = {
+//   name: "pizza",
+//   showInfo: function () {
+//     console.log(`I like to drink ${this.name} after I wake up`);
+//   },
+// };
+
+// const drink = { name: "coffee" };
+// const drink1 = { name: "juice" };
+
+// food.showInfo.call(drink);
+// food.showInfo.call(drink1);
+
+// apply is used to execute the code immediately with the list of array value
+
+// let numbers = [2, 4, 5, 28, 29];
+
+// const maxNum = Math.max.apply(null, numbers);
+// const minNum = Math.min.apply(null, numbers);
+// console.log(maxNum);
+// console.log(minNum);
+
+// let fruits = ["apple, mango"];
+// let morefruits = ["grapes, banana"];
+
+// const newList = fruits.push.apply(fruits, morefruits);
+// console.log(newList);
+// console.log(fruits);
+
+//bind
+
+function calDiscount(price, discount) {
+  return price - price * discount;
 }
-//argument = individually
-greet.call(person, "frontend", 23);
-greet.call(person2, "backend", 24);
-//arguments in an array
-greet.apply(person, ["fullstack", 74]);
 
-// bind - it has to be referred to a new variable
-const greetPerson = greet.bind(person);
-greetPerson("java", 50);
+const tenPercentDis = calDiscount.bind(null, 0.1);
+
+console.log(tenPercentDis(100));
